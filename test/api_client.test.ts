c064b10ae153d254a901cbb23763db16d4aae99c
Hernan Tain <htain@trustar.co>
2020-08-31 16:23:22 -0300

    Testing Headers

--- test/api_client.test.ts
+++ test/api_client.test.ts
@@ -7,7 +7,7 @@ const chaiAsPromised = require("chai-as-promised");
 const expect = chai.expect;
 chai.use(chaiAsPromised);
 
-describe("Auth", function () {
+describe("Authentication", function () {
   const AUTH_ENDPOINT: string = "https://api.trustar.co/oauth/token";
   const API_KEY: string = "TEST_API_KEY";
   const API_SECRET: string = "TEST_API_SECRET";
@@ -19,7 +19,8 @@ describe("Auth", function () {
       api_endpoint: "https://api.trustar.co/api/1.3",
       auth_endpoint: AUTH_ENDPOINT,
       client_type: "JS_SDK",
-      client_metatag: null, // change?
+      client_metatag: "TEST_METATAG",
+      client_version: "TEST_VERSION",
       verify: true,
       retry: false,
       max_wait_time: 60,
@@ -41,7 +42,7 @@ describe("Auth", function () {
     expect(token).to.equal(expected_token);
   });
 
-  it("should throw and error due to incorrect creds", async function () {
+  it("should throw an error due to incorrect creds", async function () {
     this.authApi
       .post("/oauth/token")
       .basicAuth({ user: API_KEY, pass: API_SECRET })
@@ -53,5 +54,26 @@ describe("Auth", function () {
     await expect(this.apiClient.getToken()).to.be.rejectedWith(
       "Unable to get token"
     );
+  });
+
+  it("headers should be formed successfully", async function () {
+    this.authApi
+      .post("/oauth/token")
+      .basicAuth({ user: API_KEY, pass: API_SECRET })
+      .query({ grant_type: "client_credentials" })
+      .reply(200, {
+        access_token: 1234567890,
+      });
+
+    let expected_headers: object = {
+      Authorization: "Bearer 1234567890",
+      "Client-Type": "JS_SDK",
+      "Client-Metatag": "TEST_METATAG",
+      "Client-Version": "TEST_VERSION",
+      "Content-Type": "application/json",
+    };
+
+    let headers: object = await this.apiClient.getHeaders(true);
+    expect(headers).to.be.deep.equal(expected_headers);
   });
 });
