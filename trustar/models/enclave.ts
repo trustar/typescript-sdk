9a8d138bef8d38a53af3f0abc8e759de58cf9623
Jesse Hedden <jhedden@trustar.co>
2020-09-03 15:10:05 -0700

    simplified json translation

--- trustar/models/enclave.ts
+++ trustar/models/enclave.ts
@@ -2,9 +2,12 @@ import { BaseModel } from "./base"
 
 
 export interface EnclaveJSON {
-    id: string;
-    name?: string;
-    type?: string;
+  id: string;
+  name?: string;
+  type?: string;
+  read?: boolean;
+  create?: boolean;
+  update?: boolean;
 }
 
 /**
@@ -12,60 +15,58 @@ export interface EnclaveJSON {
  */
 export class Enclave extends BaseModel {
 
-    id: string;
-    name?: string;
-    type?: string;
+  id: string;
+  name?: string;
+  type?: string;
 
 
-    /**
-     * Creates an instance of enclave.
-     * @param id The unique ID of the enclave.
-     * @param name The name of the enclave.
-     * @param type The type of the enclave. 
-     */
-    constructor({id, name, type}: {id: string, name?: string, type?: string}) {
+  /**
+   * Creates an instance of enclave.
+   * @param id The unique ID of the enclave.
+   * @param name The name of the enclave.
+   * @param type The type of the enclave. 
+   */
+  constructor(enclave: EnclaveJSON) {
 
-        super();
-        this.id = id;
-        this.name = name;
-        this.type = type;
-    }
+      super();
+      this.id = enclave.id;
+      this.name = enclave.name;
+      this.type = enclave.type;
+  }
 
-    static fromJSON(json: string): Enclave {
-        const enclaveJSON = JSON.parse(json);
-        return new Enclave({...enclaveJSON});
-    }
+  static fromJSON(json: string): Enclave {
+      const enclaveJSON = JSON.parse(json);
+      return new Enclave(enclaveJSON);
+  }
 }
-
 
 /**
  * Enclave permissions
  */
 export class EnclavePermissions extends Enclave {
 
-    name?: string;
-    type?: string;
-    read?: boolean;
-    create?: boolean;
-    update?: boolean;
+  name?: string;
+  type?: string;
+  read?: boolean;
+  create?: boolean;
+  update?: boolean;
 
 
-    /**
-     * Creates an instance of enclave permissions.
-     * @param id The unique ID of the enclave.
-     * @param name The name of the enclave.
-     * @param type The type of the enclave.
-     * @param read Whether the associated user/company has read access.
-     * @param create Whether the associated user/company has create access.
-     * @param update Whether the associated user/company has update access.
-     */
-    constructor({id, name, type, read, create, update}: {id: string, name?: string,
-        type?: string, read?: boolean, create?: boolean, update?: boolean}) {
+  /**
+   * Creates an instance of enclave permissions.
+   * @param id The unique ID of the enclave.
+   * @param name The name of the enclave.
+   * @param type The type of the enclave.
+   * @param read Whether the associated user/company has read access.
+   * @param create Whether the associated user/company has create access.
+   * @param update Whether the associated user/company has update access.
+   */
+  constructor(enclavePermissions: EnclaveJSON) {
 
-            super({id, name, type});
-            this.read = read;
-            this.create = create;
-            this.update = update;
-            
-        }
+      super(enclavePermissions);
+      this.read = enclavePermissions.read;
+      this.create = enclavePermissions.create;
+      this.update = enclavePermissions.update;
+        
+    }
 }
